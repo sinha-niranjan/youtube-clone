5700110794d7aa34b1fd37d97c6f0479e9ba45a1
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-08-18 11:51:05 +0530

    feed completed

--- src/components/LeftNavMenuitem.jsx
+++ src/components/LeftNavMenuitem.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const LeftNavMenuitem = () => {
+const LeftNavMenuitem = ({ text, icon, className, action }) => {
   return (
-    <div>
-      
+    <div
+      className={
+        "text-white text-sm cursor-pointer h-10 flex items-center  px-3 mb-[1px] rounded-lg hover:bg-white/[0.15] "+className
+      }
+      onClick={action}
+    >
+      <span className="text-xl mr-5">{icon}</span>
+      {text}
     </div>
-  )
-}
+  );
+};
 
-export default LeftNavMenuitem
+export default LeftNavMenuitem;
